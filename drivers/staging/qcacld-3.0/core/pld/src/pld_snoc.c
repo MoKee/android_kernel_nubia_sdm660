18cd229d152b75e1b73bd7c4a98d0f858371fae9
Cyborg2017 <wangyang0987654@gmail.com>
2019-09-08 10:49:55 +0800

    drivers: staging: qcacld-3.0: Ignore unused function

--- drivers/staging/qcacld-3.0/core/pld/src/pld_snoc.c
+++ drivers/staging/qcacld-3.0/core/pld/src/pld_snoc.c
@@ -263,9 +263,13 @@ static int pld_snoc_uevent(struct device *dev,
 		data.uevent = PLD_FW_DOWN;
 		data.fw_down.crashed = uevent_data->crashed;
 		break;
+#ifdef ICNSS_UEVENT_FW_READY
 	case ICNSS_UEVENT_FW_READY:
 		data.uevent = PLD_FW_READY;
 		break;
+#else
+
+#endif
 
 	default:
 		return 0;
